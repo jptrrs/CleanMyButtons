9ac1da0638b4a3fc34c4efbc3e6068abcf1918fa
João Pedro Torres <jaopedro05@outlook.com>
2021-05-19 14:22:18 -0300

    Add project files.

---
+++ CleanButtons/Command_LabelCap.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace CleanButtons
+{
+    [HarmonyPatch(typeof(Command), nameof(Command.LabelCap), MethodType.Getter)]
+    public static class Command_LabelCap
+    {
+        public static Dictionary<Command, Rect> Buttons = new Dictionary<Command, Rect>();
+        static string Postfix(string label, Command __instance)
+        {
+            return Buttons.EnumerableNullOrEmpty() || (Buttons.ContainsKey(__instance) && Mouse.IsOver(Buttons[__instance])) ? label : null;
+        }
+    }
+
+    [HarmonyPatch(typeof(Command), nameof(Command.GizmoOnGUIInt))]
+    public static class Command_GizmoOnGUIInt
+    {
+        public static void Prefix(Command __instance, Rect butRect)
+        {
+            if (!Command_LabelCap.Buttons.ContainsKey(__instance)) Command_LabelCap.Buttons.Add(__instance, butRect);
+        }
+        public static void Postfix(Command __instance)
+        {
+            if (Command_LabelCap.Buttons.ContainsKey(__instance)) Command_LabelCap.Buttons.Remove(__instance);
+        }
+    }
+}
---
+++ CleanButtons/HarmonyPatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace CleanButtons
+{
+    [StaticConstructorOnStartup]
+    public class HarmonyPatcher
+    {
+        static HarmonyPatcher()
+        {
+            var harmony = new Harmony("JPT.CleanButtons");
+            harmony.PatchAll();
+        }
+    }
+}
